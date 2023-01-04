13b9e5a194ffa85b6e9d8833d9b157e31b651bb6
JoeyMBrown <brnbunch45@yahoo.com>
2023-01-04 18:39:33 -0500

    Revert "README.md getting started section this reverts .next folder and unnecessary build files."
    This reverts commit 787fd37d2f0acfa2936d02e97f3f15ba9573896e.

--- .next/static/6LOoIDo0e82011YROwzcC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/760-2e39e9c9cd9050fd.js","static/chunks/pages/index-fb512b41a226116c.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-# Joey's Portfolio Site
-
-### :tada: Getting Started :tada:
-
-1. Clone down this repository.
-2. Cd into local repo root folder
-3.   `npm install`
-4. Checkout git branch `git checkout -b feature/branch-name`
-5.  `npm run build`
-6. `npm run start`
+hi
