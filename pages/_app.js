89e16dcca67c0442b0f52e2a062bdb56322f628e
JoeyMBrown <brnbunch45@yahoo.com>
2023-01-10 01:38:55 -0500

    Import devicon style sheet for language icons.  Add 2 skills to skills section, change all icons to lanaguage specific icons.

--- pages/_app.js
+++ pages/_app.js
@@ -56,6 +56,10 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
+         rel="stylesheet"
+        />
       </Head>
       {load && <PreLoader />}
       <Component {...pageProps} />
--- src/components/Services.js
+++ src/components/Services.js
@@ -18,46 +18,24 @@ const Services = () => {
         <div className="row">
           <div className="col-sm-6 m-15px-tb">
             <div className="feature-box-01 media">
-              <i className="icon theme-bg icon-mobile" />
+              <i class=" theme-bg icon-mobile icon devicon-react-original-wordmark colored"></i>
+              {/*<i className="icon theme-bg icon-mobile" />*/}
               <div className="feature-content media-body">
                 <h5>React</h5>
                 <p>
                   Lambda School gave me professional React experience
-                  while leading small team, non-profit work. React is my preffered
-                  JS framework.
+                  while leading small team non-profit work. React is my go-to
+                  JS library.
                 </p>
               </div>
             </div>
           </div>
           <div className="col-sm-6 m-15px-tb">
             <div className="feature-box-01 media">
-              <i className="icon theme-bg icon-desktop" />
+              <i class="icon theme-bg devicon-nodejs-plain colored"></i>
+              {/*<i className="icon theme-bg icon-desktop" />*/}
               <div className="feature-content media-body">
                 <h5>Node</h5>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-6 m-15px-tb">
-            <div className="feature-box-01 media">
-              <i className="icon theme-bg icon-target" />
-              <div className="feature-content media-body">
-                <h5>Javascript</h5>
-                <p>
-                  Javascript is the first language I studied.  Learning
-                  Javascript was love at first sight, and lead to my first ever project - a simple calculator.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-6 m-15px-tb">
-            <div className="feature-box-01 media">
-              <i className="icon theme-bg icon-mobile" />
-              <div className="feature-content media-body">
-                <h5>ASP.NET</h5>
                 <p>
                   
                 </p>
@@ -66,7 +44,34 @@ const Services = () => {
           </div>
           <div className="col-sm-6 m-15px-tb">
             <div className="feature-box-01 media">
-              <i className="icon theme-bg icon-desktop" />
+            <i class="icon devicon-javascript-plain colored theme-bg icon-mobile" />
+              {/*<i className="icon theme-bg icon-target" />*/}
+              <div className="feature-content media-body">
+                <h5>Javascript</h5>
+                <p>
+                  Javascript is the first language I studied.  Learning
+                  Javascript was love at first sight, and lead to my first
+                  ever project - a simple calculator.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 m-15px-tb">
+            <div className="feature-box-01 media">
+              {/*<i className="icon theme-bg icon-mobile" />*/}
+              <i class="icon theme-bg devicon-vuejs-plain-wordmark colored"></i>
+              <div className="feature-content media-body">
+                <h5>Vue.js</h5>
+                <p>
+                  
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 m-15px-tb">
+            <div className="feature-box-01 media">
+              <i class="icon theme-bg devicon-csharp-plain colored"></i>
+              {/*<i className="icon theme-bg icon-desktop" />*/}
               <div className="feature-content media-body">
                 <h5>C#</h5>
                 <p>
@@ -77,9 +82,34 @@ const Services = () => {
           </div>
           <div className="col-sm-6 m-15px-tb">
             <div className="feature-box-01 media">
-              <i className="icon theme-bg icon-target" />
+              {/*<i className="icon theme-bg icon-target" />*/}
+              <i class="icon theme-bg devicon-postgresql-plain-wordmark colored"></i>
               <div className="feature-content media-body">
-                <h5>REST APIs</h5>
+                <h5>PostgreSQL</h5>
+                <p>
+                  
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 m-15px-tb">
+            <div className="feature-box-01 media">
+              {/*<i className="icon theme-bg icon-target" />*/}
+              <i class="icon theme-bg devicon-html5-plain colored"></i>
+              <div className="feature-content media-body">
+                <h5>HTML5</h5>
+                <p>
+                  
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 m-15px-tb">
+            <div className="feature-box-01 media">
+              {/*<i className="icon theme-bg icon-target" />*/}
+              <i class="icon theme-bg devicon-css3-plain-wordmark colored"></i>
+              <div className="feature-content media-body">
+                <h5>CSS</h5>
                 <p>
                   
                 </p>
